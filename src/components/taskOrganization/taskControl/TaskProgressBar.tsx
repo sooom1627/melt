1e6c565ff8d0aee1f29f5a3f844c3f850ffd22dc
soma <swfh2367@gmail.com>
2023-08-06 22:56:13 +0900

    refactor task progress logic

--- src/components/taskOrganization/taskControl/TaskProgressBar.tsx
+++ src/components/taskOrganization/taskControl/TaskProgressBar.tsx
@@ -10,79 +10,71 @@ type TimeSegment = {
 	percentage: number;
 };
 
+const calculateTotalDuration = (task: Task) => {
+	const now = new Date();
+	return task.start ? now.getTime() - task.start.getTime() : 0;
+};
+
+const generateSegmentsForTask = (task: Task, totalDuration: number) => {
+	let tempSegments: TimeSegment[] = [];
+	let previousEndTime = task.start ? task.start.getTime() : 0;
+
+	task.pauses?.forEach((pause) => {
+		if (pause.start && pause.end) {
+			const workSegmentPercentage =
+				((pause.start.getTime() - previousEndTime) / totalDuration) * 100;
+			tempSegments.push({
+				type: "work",
+				percentage: workSegmentPercentage,
+			});
+
+			const pauseSegmentPercentage =
+				((pause.end.getTime() - pause.start.getTime()) / totalDuration) * 100;
+			tempSegments.push({
+				type: "pause",
+				percentage: pauseSegmentPercentage,
+			});
+
+			previousEndTime = pause.end.getTime();
+		}
+	});
+
+	const finalPercentage =
+		((new Date().getTime() - previousEndTime) / totalDuration) * 100;
+	tempSegments.push({
+		type: "work",
+		percentage: finalPercentage,
+	});
+
+	return tempSegments;
+};
+
 export const TaskProgressBar = () => {
 	const selectedTask = useRecoilValue(selectedTaskState);
 	const [segments, setSegments] = useState<TimeSegment[]>([]);
 
-	const updateSegments = (selectedTask: Task) => {
-		if (selectedTask) {
-			const now = new Date();
-			let totalDuration = 0;
-			let previousEndTime = 0;
-			const tempSegments: TimeSegment[] = [];
-			if (selectedTask.start) {
-				totalDuration = now.getTime() - selectedTask.start.getTime();
-				previousEndTime = selectedTask.start.getTime();
-			}
-
-			// selectedTask.pausesが存在して、その長さが0より大きい場合のみ
-			if (
-				totalDuration &&
-				selectedTask.pauses &&
-				selectedTask.pauses.length > 0
-			) {
-				selectedTask.pauses.forEach((pause) => {
-					if (pause.start && pause.end) {
-						const workSegmentPercentage =
-							((pause.start.getTime() - previousEndTime) / totalDuration) * 100;
-						tempSegments.push({
-							type: "work",
-							percentage: workSegmentPercentage,
-						});
-
-						const pauseSegmentPercentage =
-							((pause.end.getTime() - pause.start.getTime()) / totalDuration) *
-							100;
-						tempSegments.push({
-							type: "pause",
-							percentage: pauseSegmentPercentage,
-						});
-
-						previousEndTime = pause.end.getTime();
-					}
-				});
-
-				const finalWorkSegmentPercentage =
-					((now.getTime() - previousEndTime) / totalDuration) * 100;
-				tempSegments.push({
-					type: "work",
-					percentage: finalWorkSegmentPercentage,
-				});
-			} else {
-				// pausesが存在しない、もしくは空の配列の場合、またはselectedTask.startが存在しない場合
-				// 0%の作業時間もしくは100%の作業時間として追加
-				tempSegments.push({
-					type: "work",
-					percentage: selectedTask.start ? 100 : 0,
-				});
-			}
-
-			setSegments(tempSegments);
-		}
-	};
-
 	useEffect(() => {
-		// タスクが開始または一時停止中の場合のみタイマーを設定
-		if (selectedTask?.status === "started") {
-			// 定期的にセグメントを再計算
-			const interval = setInterval(() => {
-				updateSegments(selectedTask); // 下記で定義する関数
-			}, 1000);
+		const updateSegmentsForSelectedTask = () => {
+			if (selectedTask) {
+				const totalDuration = calculateTotalDuration(selectedTask);
 
-			// クリーンアップ時にタイマーをクリア
-			return () => clearInterval(interval);
+				if (totalDuration) {
+					const newSegments = generateSegmentsForTask(
+						selectedTask,
+						totalDuration
+					);
+					setSegments(newSegments);
+				}
+			}
+		};
+
+		if (selectedTask?.status === "started") {
+			updateSegmentsForSelectedTask(); // 最初に一回実行
+			const interval = setInterval(updateSegmentsForSelectedTask, 1000); // 1秒ごとの更新
+
+			return () => clearInterval(interval); // クリーンアップ時にタイマーをクリア
 		} else if (selectedTask?.status) {
-			updateSegments(selectedTask);
+			updateSegmentsForSelectedTask(); // 一度だけ実行
 		}
 	}, [selectedTask]);
 
